69e820d2bdadc5932332f4a3e0d55b7be8ce7aa0
Gleb <kovgleb04@gmail.com>
2024-10-18 23:03:44 +0300

    Initial Commit

---
+++ TrainingApp/TrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingAppApp.swift
+//  TrainingApp
+//
+//  Created by Kovalev Gleb on 18.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
