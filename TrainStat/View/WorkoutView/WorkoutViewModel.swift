7edf541ab9f1286832887f63ac008334079fbd27
gordei228 <vakhrushevgp239@gmail.com>
2024-11-22 23:14:26 +0300

    workoutIntensivityResult

--- TrainStat/View/WorkoutView/WorkoutViewModel.swift
+++ TrainStat/View/WorkoutView/WorkoutViewModel.swift
@@ -33,11 +33,20 @@ class WorkoutViewModel: ObservableObject {
     // MARK: FIX
     // workoutIntensivity fix -> set it
     // right now const 0
+    func workoutIntensivityResult() -> Int64 {
+        var result = 0.0
+        workoutModel.exersises.forEach {exercise in
+            exercise.sets.forEach {set in
+                result += set.weight * Double(set.repeats)
+            }
+        }
+        return Int64(result)
+    }
     func endWorkoutButtonTapped() {
         workoutModel.time = "\(timeString)"
         stopTimer()
         workoutModel.date = Date()
-        workoutModel.intensivity = 0
+        workoutModel.intensivity = workoutIntensivityResult()
         saveWorkoutInCoreData()
         workoutResultScreen = true
     }
